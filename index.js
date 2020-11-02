e3b849e776c4c7b0ab6f35a78547e5871e905005
nghiemtuan <nghiem.tien.tuan@sun-asterisk.com>
2020-11-02 19:47:47 +0700

    add console in post

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/webhook', function(req, res) {
     if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
         res.send(req.query['hub.challenge']);
     }
+
     res.send('Error, wrong validation token');
 });
 
@@ -31,19 +32,20 @@ app.post('/webhook', function(req, res) {
     console.log('webhook post');
 
     var entries = req.body.entry;
-    for (var entry of entries) {
-        var messaging = entry.messaging;
-        for (var message of messaging) {
-            var senderId = message.sender.id;
-            if (message.message) {
-                if (message.message.text) {
-                    var text = message.message.text;
-                    sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
-                }
-            }
-        }
-    }
-    res.status(200).send("OK");
+    console.log(entries);
+    // for (var entry of entries) {
+    //     var messaging = entry.messaging;
+    //     for (var message of messaging) {
+    //         var senderId = message.sender.id;
+    //         if (message.message) {
+    //             if (message.message.text) {
+    //                 var text = message.message.text;
+    //                 sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
+    //             }
+    //         }
+    //     }
+    // }
+    // res.status(200).send("OK");
 });
 
 function sendMessage(senderId, message) {

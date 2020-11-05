77d5c19973e2f6c35b30bd71139f2271386132ad
nghiemtuan <nghiem.tien.tuan@sun-asterisk.com>
2020-11-05 16:14:43 +0700

    add log message 2

--- src/helper/chatterbot.js
+++ src/helper/chatterbot.js
@@ -3,6 +3,7 @@ const path = require('path');
 const chatterBotResponsePath = path.join(__dirname, '/../chatterbot/chatterBotResponse.py');
 
 function callChatterBot(options = null) {
+	console.log(options);
     return new PythonShell(chatterBotResponsePath, options);
 }
 

b5b5976d60b0b789be472407adb8e45aa8f29f05
nghiemtuan <nghiem.tien.tuan@sun-asterisk.com>
2020-11-05 16:32:37 +0700

    add log message 5

--- src/helper/chatterbot.js
+++ src/helper/chatterbot.js
@@ -2,10 +2,11 @@ const {PythonShell} = require('python-shell');
 const path = require('path');
 const chatterBotResponsePath = path.join(__dirname, '/../chatterbot/chatterBotResponse.py');
 
-function callChatterBot(options = null) {
-	return await new Promise((resolve, reject) => {
-	PythonShell.run(chatterBotResponsePath, options, (err, results) => {
-	    if (err) return reject(err);
+async function  callChatterBot(options = null) {
+	return new await Promise((resolve, reject) => {
+		PythonShell.run(chatterBotResponsePath, options, (err, results) => {
+	    	if (err) return reject(err);
+
 	        return resolve(results);
 	    });
 	});
